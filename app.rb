1b89e7c9a60021e25b36a3c78ee2d9cfa2931440
Andrew Waller <andrewhwaller@gmail.com>
2019-04-21 23:40:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,13 +4,13 @@ class App < Sinatra::Base
 
   get '/newteam' do
     erb :newteam
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @pf = params[:pf]
-    @sf = params[:sf]
-    @c = params[:c]
+    # @name = params[:name]
+    # @coach = params[:coach]
+    # @pg = params[:pg]
+    # @sg = params[:sg]
+    # @pf = params[:pf]
+    # @sf = params[:sf]
+    # @c = params[:c]
   end
 
   # post '/newteam' do
